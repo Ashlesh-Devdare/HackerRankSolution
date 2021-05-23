385c63c99262846b4be1c0e2ab51ad7097f31a03
ashlesh <ashlesh@hp-lappy>
2021-05-23 15:46:17 +0530

    First commit for the code

---
+++ src/prgmr/ashlesh/thnksmart/hcrrnk/Client.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package prgmr.ashlesh.thnksmart.hcrrnk;
+
+/**
+ * @author ashlesh
+ *
+ */
+public class Client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Running Java program from a while");
+	}
+
+}
